550308a8213b086a168d2874e34b338ca6b4cd59
lagiacruskiller <Ceadues2>
2020-01-07 15:01:32 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,13 +2,44 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView arrow;
+    TextView namebox, age, gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nickname);
+        findviews();
+        arrow.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                namebox.setText("something");
+                if(TextUtils.isEmpty(namebox.getText())) {
+                    startActivity(new Intent(MainActivity.this, NicknameActivity.class));
+                }
+                else{
+                    String userid = getSharedPreferences("test", MODE_PRIVATE)
+                            .getString("USER", "");
+                    namebox.setText(userid);
+                }
+            }
+        });
+    }
+
+    public void findviews(){
+        arrow = findViewById(R.id.arrow);
+        namebox = findViewById(R.id.namebox);
+        age = findViewById(R.id.agebox);
+        gender = findViewById(R.id.genderbox);
     }
 }
--- app/src/main/java/com/fju/member/NicknameActivity.java
+++ app/src/main/java/com/fju/member/NicknameActivity.java
@@ -1,14 +1,42 @@
 package com.fju.member;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class NicknameActivity extends AppCompatActivity {
-
+    ImageView arrow = findViewById(R.id.arrow);
+    EditText nickname = findViewById(R.id.nickname);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nickname);
+
+        arrow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(TextUtils.isEmpty(nickname.getText())) {
+                    new AlertDialog.Builder(NicknameActivity.this)
+                            .setTitle("Empty Information")
+                            .setMessage("Please input nickname info")
+                            .show();
+                }
+                else{
+                    String user = nickname.getEditableText().toString();
+                    SharedPreferences pref = getSharedPreferences("name", MODE_PRIVATE);
+                    pref.edit()
+                            .putString("USER", user)
+                            .commit();
+                }
+            }
+        });
+
+
     }
 }
