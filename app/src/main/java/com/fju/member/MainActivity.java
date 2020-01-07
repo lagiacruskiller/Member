0574d26e04bb026ffd1519ff149fe31ccff8bbc4
lagiacruskiller <Ceadues2>
2020-01-07 15:35:54 +0800

    I don't know why it doesn't want to run at all

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nickname);
+        setContentView(R.layout.activity_main);
         findviews();
         arrow.setOnClickListener(new Button.OnClickListener(){
             @Override
